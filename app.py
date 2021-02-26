99648dc93de8ff522f9ed9686ae4c8f0affde4c4
PooiaFerdowsi <76428946+PooiaFerdowsi@users.noreply.github.com>
2021-02-26 12:27:59 +0330

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import ( 
-  Flask , render_template )
+  Flask , render_template 
+  request
+)
 
 app = Flask("__main__") # also can ve Flask(__name__)
 
@@ -10,6 +12,14 @@ def index():
 @app.route('/user/int:<id>')
 def profile(user:int):
   return f"Hello The User with {user} ID"
+
+@app.route("/login",methods=['GET','POST'])
+def login():
+  return "<form action='/log' method='GET'><input type='text' name='pswd'/></form>"
+
+@app.route('/log')
+def log():
+  return request.args.get("pswd")
 
 if __name__ == "__main__":
   app.run(debug=True,host="0.0.0.0",port="5000")
