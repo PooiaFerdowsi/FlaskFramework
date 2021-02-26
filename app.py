7155e997ff821edf46ee2969cd8794c9d15586b8
PooiaFerdowsi <76428946+PooiaFerdowsi@users.noreply.github.com>
2021-02-26 12:24:16 +0330

    Update app.py

--- app.py
+++ app.py
@@ -6,3 +6,10 @@ app = Flask("__main__") # also can ve Flask(__name__)
 @app.route('/')
 def index():
   return render_template("index.htm")
+
+@app.route('/user/int:<id>')
+def profile(user:int):
+  return f"Hello The User with {user} ID"
+
+if __name__ == "__main__":
+  app.run(debug=True,host="0.0.0.0",port="5000")
